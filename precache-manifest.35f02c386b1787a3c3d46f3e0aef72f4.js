309d00d9e7c58de4a5a9eaa7f603b27b092892bd
Tak Tran <contact@tutaktran.com>
2019-05-28 22:51:12 +0100

    Updates

---
+++ precache-manifest.35f02c386b1787a3c3d46f3e0aef72f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142f37ec013c6e36943e0d3036bfbd20",
+    "url": "/joy-joy/index.html"
+  },
+  {
+    "revision": "39e11f9eba2b1cdd9078",
+    "url": "/joy-joy/static/css/main.b269aba3.chunk.css"
+  },
+  {
+    "revision": "009cd341132490709493",
+    "url": "/joy-joy/static/js/2.02268aef.chunk.js"
+  },
+  {
+    "revision": "39e11f9eba2b1cdd9078",
+    "url": "/joy-joy/static/js/main.5fa4d869.chunk.js"
+  },
+  {
+    "revision": "44a7d4ff9406e0a50845",
+    "url": "/joy-joy/static/js/runtime~main.fec017f3.js"
+  }
+]);
